0010ed2d7b3ede16417cdfa17188b0e7b0dddb57
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-01-17 16:00:10 +0530

    Create function for viewing products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . models import ProductInfo
 # Create your views here.
 
 def index(request):
@@ -7,7 +7,9 @@ def index(request):
     return render(request,'index.html')
 
 def products(request):
-    return render(request,'products.html')
+    product_list= ProductInfo.objects.all()
+    list={'products':product_list}
+    return render(request,'products.html',list)
 
 def product_info(request):
     return render(request,'product_details.html')
